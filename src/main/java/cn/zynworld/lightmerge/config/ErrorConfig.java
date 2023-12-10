1b4babe19c79236b7e528286c55ad169da880886
buynowdev <zhaozhaozhao1996@163.com>
2023-12-10 18:33:13 +0800

    fix: router

---
+++ src/main/java/cn/zynworld/lightmerge/config/ErrorConfig.java
@@ -0,0 +1,17 @@
+package cn.zynworld.lightmerge.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ErrorConfig implements ErrorPageRegistrar {
+
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+        registry.addErrorPages(error404Page);
+    }
+}
